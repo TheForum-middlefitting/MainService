39f7450f743afb7082c3fb1b0f87793ceb95ce2f
middlefitting <wjdtmdcjf2@gmail.com>
2022-07-02 12:14:56 +0900

    Initialize

---
+++ src/test/java/com/practice/springbasic/SpringbasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.springbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
